29735a4d8bff34a471b5351799a953d992e3d336
jisun0405 <jiseoniii@naver.com>
2017-11-03 14:33:31 +0900

    LocalClass Test

---
+++ src/lesson7/AbstractClassTest.java
@@ -0,0 +1,33 @@
+package lesson7;
+
+public class AbstractClassTest {
+
+    static abstract class Person{
+        private String name;
+        public Person(){this.name="";}
+        public Person(String name){this.name = name;}
+        public final String getName(){return name;}
+
+        public abstract int getId();
+    }
+    static class Student extends Person{
+        private int id;
+        public Student(String name, int id){
+            //super()
+            this.id = id;
+        }
+
+        @Override
+        public int getId() {
+            return 0;
+        }
+    }
+    static public void main(String[] arg){
+        Person p = new Person('hika'){
+            @Override
+            public int getId() {
+                return 0;
+            }
+        };
+    }
+}
