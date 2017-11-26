b346cbf0ab0a55c1ade9ffda9aa97345bf5f3a6f
jisun0405 <jiseoniii@naver.com>
2017-11-26 21:32:17 +0900

    11/26

--- src/com/jisun0405/TodoList.java
+++ src/com/jisun0405/TodoList.java
@@ -1,4 +1,13 @@
 package com.jisun0405;
 
+import java.util.ArrayList;
+
 public class TodoList {
+    public String todoListName;
+    ArrayList<ArrayList<String>> tasks = new ArrayList<>();
+
+    public TodoList(String todoListName){
+        this.todoListName = todoListName;
+
+    }
 }
